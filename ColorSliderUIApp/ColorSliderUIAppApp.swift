24422e296bc85987f14dcddcc08a83469831dbd6
Ula <1295593@mail.ru>
2022-03-29 15:36:23 +0800

    Initial Commit

---
+++ ColorSliderUIApp/ColorSliderUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliderUIAppApp.swift
+//  ColorSliderUIApp
+//
+//  Created by Юлия Алдохина on 29/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliderUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
